60e353388431c183c9116f28690f296a68ddecda
Arthur-H-G-Wild <arthurwild04@gmail.com>
2026-06-02 20:56:03 -0300

    Validação e correção da lógica de Golomb

--- validacao.py
+++ validacao.py
@@ -2,4 +2,11 @@ from modulos.golomb import Golomb
 from modulos.fibonacci import Fibonacci
 from modulos.eliasgamma  import EliasGamma
 from modulos.huffman import NoHuffman
-import modulos.utils
+import modulos.utils as utils
+
+mensagem = "Teste para validação de codificações"
+
+encode = Golomb.golomb_encode(mensagem, 64)
+print("Golomb:", encode)
+decode = Golomb.golomb_decode(encode, 64)
+print("Golomb Decodificado:", decode)
